ec57769e1183ca3c19d7d9ceecb2463d6ae808d2
NguyenThanhVien <nguyenthanhvienaf@gmail.com>
2024-06-14 12:20:37 +0700

    Add file word bao cao, file ppt thuyet trinh va file zip source code

--- ShoeStore_Nhom2_Web2/obj/Debug/net8.0/ShoeStore_Nhom2_Web2.AssemblyInfo.cs
+++ ShoeStore_Nhom2_Web2/obj/Debug/net8.0/ShoeStore_Nhom2_Web2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeStore_Nhom2_Web2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a692c13fac577206c887c54383edd509697c59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeStore_Nhom2_Web2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoeStore_Nhom2_Web2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
